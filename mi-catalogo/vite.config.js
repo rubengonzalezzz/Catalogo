9006b8dc71d7a106a36f86b8299c7b6a81ec839c
rubengonzalezzz <inge.rubengonzalez@gmail.com>
2026-03-20 01:54:39 +0000

    refactor: simplify Vite configuration by removing unnecessary imports

--- mi-catalogo/vite.config.js
+++ mi-catalogo/vite.config.js
@@ -1,5 +1,3 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  base: './', // Esto hace que las rutas funcionen en cualquier subcarpeta
-})
+export default {
+  base: './' 
+}
